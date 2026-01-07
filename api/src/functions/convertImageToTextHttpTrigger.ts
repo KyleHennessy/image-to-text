c7f6bb057a2873f049070c90370baa2283a95e77
kyle hennessy <kyle-hennessy@hotmail.com>
2026-01-07 20:47:44 +1100

    Fixed bug where function would hang if file uploaded was not an image. Now validating that the file is an image before proceeding

--- api/src/functions/convertImageToTextHttpTrigger.ts
+++ api/src/functions/convertImageToTextHttpTrigger.ts
@@ -1,11 +1,7 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-
-import {
-    ImageAnalysisClient,
-    ImageAnalysisResultOutput
-} from "@azure-rest/ai-vision-image-analysis";
+import { ImageAnalysisClient, ImageAnalysisResultOutput} from "@azure-rest/ai-vision-image-analysis";
+import { fileTypeFromBuffer } from 'file-type';
 import { error } from "console";
-
 
 const { ImageAnalysisClient } = require('@azure-rest/ai-vision-image-analysis');
 const createClient = require('@azure-rest/ai-vision-image-analysis').default;
@@ -29,33 +25,44 @@ export async function convertImageToTextHttpTrigger(request: HttpRequest, contex
     const credential = new AzureKeyCredential(key);
     const client: ImageAnalysisClient = createClient(endpoint, credential);
 
-    const imageBuffer = await request.arrayBuffer();
+    const buffer = await request.arrayBuffer();
 
-    if (!imageBuffer) {
+    if (!buffer) {
         context.error("Missing Image");
         return {
-            status: 500,
+            status: 400,
             jsonBody: {
                 error: "Missing image."
             }
         }
     }
 
+    const type = await fileTypeFromBuffer(buffer);
+
+    if(!type || !type.mime.startsWith('image/')){
+        context.error('File is not an image');
+        return{
+            status:400,
+            jsonBody:{
+                error: 'File is not an image'
+            }
+        };
+    }
 
     const response = await client.path('/imageanalysis:analyze').post({
-        body: new Uint8Array(imageBuffer),
+        body: new Uint8Array(buffer),
         contentType: 'application/octet-stream',
         queryParameters: {
             features: ['read']
         },
     });
 
-    const result = response.body as ImageAnalysisResultOutput;
+    const result = response?.body as ImageAnalysisResultOutput;
 
     let output = '';
 
-    for (const block of result.readResult.blocks) {
-        for (const line of block.lines) {
+    for (const block of result?.readResult?.blocks) {
+        for (const line of block?.lines) {
             output += `${line.text}\n`;
         }
     }
