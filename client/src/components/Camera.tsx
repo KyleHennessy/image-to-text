bba790e08d62acf9fe7bcfb1030a85222adc2505
kyle hennessy <kyle-hennessy@hotmail.com>
2026-01-08 00:02:47 +1100

    Can now select a file from computer instead of snapping a photo from webcam

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -10,26 +10,46 @@ import { CgSpinner } from "react-icons/cg";
 
 export function Camera() {
   const webcamRef = useRef<Webcam>(null);
-  const [image, setImage] = useState('');
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  const [image, setImage] = useState("");
   const [uploading, setUploading] = useState(false);
 
   const onCapture = useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
-    setImage(imageSrc ?? '');
+    setImage(imageSrc ?? "");
   }, [webcamRef, setImage]);
+
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event?.target?.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64String = reader.result as string;
+      setImage(base64String);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleUploadClick = () => {
+    fileInput.current?.click();
+  };
 
   async function analyzeImage() {
     setUploading(true);
     const url =
-      'https://ai-sandbox-functions.azurewebsites.net/api/convertImageToTextHttpTrigger';
+      "https://ai-sandbox-functions.azurewebsites.net/api/convertImageToTextHttpTrigger";
 
     const blob = await (await fetch(image)).blob();
 
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/octet-stream',
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/octet-stream",
       },
       body: blob,
     });
@@ -37,7 +57,7 @@ export function Camera() {
     const json = await response.json();
 
     setUploading(false);
-    setImage('');
+    setImage("");
 
     if (!response.ok) {
       alert(json.error);
@@ -49,7 +69,8 @@ export function Camera() {
 
   return (
     <>
-      <div className={styles['webcam-container']}>
+      <div className={styles["webcam-container"]}>
+        <h1>{image}</h1>
         {!image ? (
           <Webcam
             audio={false}
@@ -65,13 +86,13 @@ export function Camera() {
               {!uploading && (
                 <>
                   <button
-                    className={`${styles['check-button']} ${styles['rounded-button']}`}
+                    className={`${styles["check-button"]} ${styles["rounded-button"]}`}
                     onClick={() => analyzeImage()}
                   >
                     <IoMdCheckmark />
                   </button>
                   <button
-                    className={`${styles['cancel-button']} ${styles['rounded-button']}`}
+                    className={`${styles["cancel-button"]} ${styles["rounded-button"]}`}
                     onClick={() => setImage("")}
                   >
                     <IoMdClose />
@@ -83,21 +104,32 @@ export function Camera() {
           </>
         )}
       </div>
-
-      <div className={styles.dock}>
-        <button>
-          <MdTextSnippet />
-        </button>
-        <button
-          className={`${styles["camera-button"]} ${styles["rounded-button"]}`}
-          onClick={() => onCapture()}
+      {!image && (
+        <div
+          className={styles.dock}
+          style={{ display: image !== "" ? "none" : "auto" }}
         >
-          <MdOutlineCameraAlt />
-        </button>
-        <button>
-          <MdUpload />
-        </button>
-      </div>
+          <button>
+            <MdTextSnippet />
+          </button>
+          <button
+            className={`${styles["camera-button"]} ${styles["rounded-button"]}`}
+            onClick={() => onCapture()}
+          >
+            <MdOutlineCameraAlt />
+          </button>
+          <button onClick={() => handleUploadClick()}>
+            <MdUpload />
+          </button>
+        </div>
+      )}
+
+      <input
+        type="file"
+        ref={fileInput}
+        style={{ display: "none" }}
+        onChange={(event) => handleFileSelect(event)}
+      />
     </>
   );
 }
