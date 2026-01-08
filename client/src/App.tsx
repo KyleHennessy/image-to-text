884090f8f1678781a7f5d56c13a58c60eddb4be0
kyle hennessy <kyle-hennessy@hotmail.com>
2026-01-08 22:32:54 +1100

    Implemented copy and download functionality.
    Using the react-toastify package to display toasts when copying or downloading file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,26 @@ import "bootstrap/js/dist/offcanvas";
 import { Camera } from "./components/Camera";
 import { Files } from "./components/Files";
 import { FilesProvider } from "./context/FileCacheContext";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <FilesProvider>
       <Camera />
       <Files />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+        transition={Bounce}
+      />
     </FilesProvider>
   );
 }
--- client/src/components/FileItem.tsx
+++ client/src/components/FileItem.tsx
@@ -1,23 +1,41 @@
 import type { TextFile } from "../context/FileCacheContext";
 import { MdDownload } from "react-icons/md";
 import { MdContentCopy } from "react-icons/md";
-import styles from './fileitem.module.css';
+import styles from "./fileitem.module.css";
+import { toast } from "react-toastify";
 
 type FileItemProps = {
   textFile: TextFile;
 };
 
 export function FileItem({ textFile }: FileItemProps) {
+  const copyText = () => {
+    navigator.clipboard.writeText(textFile.content);
+    toast("📝 Text copied");
+  };
+
+  const downloadFile = () => {
+    const blob = new Blob([textFile.content], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = textFile.date;
+    a.click();
+
+    URL.revokeObjectURL(url);
+    toast.success("File downloaded");
+  };
   return (
     <div className={styles["card"]}>
       <div className={styles["card-header"]}>
         <h4 className={styles["card-title"]}>{textFile.date.split("T")[0]}</h4>
 
         <div className={styles["card-actions"]}>
-          <button className={styles["actions"]}>
+          <button onClick={() => copyText()} className={styles["actions"]}>
             <MdContentCopy />
           </button>
-          <button className={styles["actions"]}>
+          <button onClick={() => downloadFile()} className={styles["actions"]}>
             <MdDownload />
           </button>
         </div>
