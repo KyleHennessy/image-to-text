2b82a17a425ad26d7dd61d1a303f307452e626f6
kyle hennessy <kyle-hennessy@hotmail.com>
2026-01-08 19:44:55 +1100

    Fixed styling of offcanvas body and close button.

--- client/src/components/Files.tsx
+++ client/src/components/Files.tsx
@@ -1,32 +1,22 @@
 import { useEffect } from "react";
 import { useFiles } from "../context/FileCacheContext";
+import { IoMdClose } from "react-icons/io";
 
 export function Files() {
-  const {files, setFiles} = useFiles();
+  const files = useFiles().files;
 
-  useEffect(() => {
-    console.log('files updated');
-    console.log(files);
-  }, [files]);
   return (
-    <div
-      className="offcanvas offcanvas-start"
-      id="files"
-    >
+    <div className="offcanvas offcanvas-start" id="files">
       <div className="offcanvas-header">
         <h4 className="offcanvas-title" id="offcanvasExampleLabel">
           Most Recent Images Converted To Text
         </h4>
-        <button
-          type="button"
-          className="btn-close"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div className="offcanvas-body">
+        
+
+          <IoMdClose className="close-offcanvas" data-bs-dismiss="offcanvas"  />
         
       </div>
+      <div className="offcanvas-body"></div>
     </div>
   );
 }
